e3acf5c4066391103f2a57c19f23a393bdcafa63
DominiqueYoumans <ddyoumans@icloud.com>
2020-02-20 19:24:42 -0800

    add pseudo for arrays of quotes

--- Program.cs
+++ Program.cs
@@ -2,11 +2,17 @@
 
 namespace mambaQuotes
 {
-    class Program
+  class Program
     {
-        static void Main(string[] args)
+      public static Dictionary<string, string> quotes = new Dictionary
+        static void Main(string[] str1, str2;)
         {
-            Console.WriteLine("Hello World!");
+            str1 = new string[]{"I can't relate to lazy people. We don't speak the same language. I don't understand you. I don't want to understand you.", "The moment you give up is the moment you let someone else win", "The last time I was intimidated was when I was 6 years old in karate class. I was an orange belt and the instructor ordered me to fight a black belt who was a couple years older and a lot bigger. I was scared s--less. I mean, I was terrified and he kicked my ass. But then I realized he didn't kick my ass as bad as I thought he was going to and that there was nothing really to be afraid of. That was around the time I realized that intimidation didn't really exist if you're in the right frame of mind."}
+
+            str2 = new string[]{"I have self-doubt. I have insecurity. I have fear of failure. I have nights when I show up at the arena and I'm like, 'My back hurts, my feet hurt, my knees hurt. I don't have it. I just want to chill.' We all have self-doubt. You don't deny it, but you also don't capitulate to it. You embrace it.","Once you know what failure feels like, determination chases success.", "I have nothing in common with lazy people who blame others for their lack of success. Great things come from hard work and perseverance. No excuses."}
+
+
+            Console.WriteLine("Hello Kobe Bryant Fan!");
         }
     }
 }
